19664248c848bd84815716597fcbe57ade278a49
Duncan Moyo <duncanfmoyo@gmail.com>
2021-04-12 23:22:09 +0200

    Navigation to the MealDetail Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoryMealsScreen = (props) => {
   return (
-    <View style={styles.screen} >
+    <View style={styles.screen}>
       <Text>The Category Meal Screen</Text>
+      <Button
+        title="Meal Info"
+        onPress={() => {
+          props.navigation.navigate("MealDetail");
+        }}
+      />
     </View>
   );
 };
@@ -12,9 +18,9 @@ const CategoryMealsScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default CategoryMealsScreen;
