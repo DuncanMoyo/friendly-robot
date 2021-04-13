3c7525c7a49a55c9a02f91568d2eb2fb7b43a409
Duncan Moyo <duncanfmoyo@gmail.com>
2021-04-13 13:57:26 +0200

    Passing and reading params upon navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -16,7 +16,9 @@ const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
       <TouchableOpacity style={styles.gridItem} onPress={() => {
-        props.navigation.navigate({routeName: 'CategoryMeals'})
+        props.navigation.navigate('CategoryMeals', {
+          categoryId: itemData.item.id
+        })
       }} >
         <View>
           <Text>{itemData.item.title}</Text>
@@ -45,7 +47,7 @@ CategoriesScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
+    flex: 1,  
     justifyContent: "center",
     alignItems: "center",
   },
